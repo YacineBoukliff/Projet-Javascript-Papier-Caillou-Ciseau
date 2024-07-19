fb7242d07a005cf6c0b003a971b58eb1ad8bb9fd
yacine bouklif <y.bouklif@it-students.fr>
2024-07-19 12:16:21 +0200

    Ajout nouveau exercice

--- Projet JS/script.js
+++ Projet JS/script.js
@@ -1,10 +1,21 @@
-let hour = 11;
-let namee = "yacine";
+// Exo sur les variable 
 
-if (hour > 6 && hour < 12) {
-  console.log(`Good morning ${namee}`);
-} else if (hour > 12 && hour < 17) {
-  console.log(`Good afternoon ${namee}`);
-} else {
-  console.log(`Good night ${namee}`);
-}
+// Exo 1 a 5 
+
+let nom = "Yacine"
+
+// Exo 2 
+
+console.log(`Ton nom est : ${nom}`)
+
+// Exo 3 et 4
+
+const cafe = 5
+const bagel = 3
+const bagel1 =3
+const soupe = 9
+
+let PrixTotal = cafe + bagel + bagel1 + soupe 
+console.log(`Le prix de tout ces aliments est de : ${PrixTotal}`)
+
+// Exo 5 
